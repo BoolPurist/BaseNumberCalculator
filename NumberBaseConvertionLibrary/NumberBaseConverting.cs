ed3049c2092acfc05812c34a03c757389bddf8f7
Florian <naumannfl79974@th-nuernberg.de>
2020-08-07 21:08:16 +0200

    Implementation of background logic for converting number according to a base.

---
+++ NumberBaseConvertionLibrary/NumberBaseConverting.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace NumberBaseConvertionLibrary
+{
+    public static class NumberBaseConverting
+    {
+        private static readonly int maxBase = 36;
+
+        public static int MaxBase => maxBase;
+
+        
+
+        public static bool IsValidNumberWithBase(string textNumber, int sourceBase)
+        {
+            if (textNumber == null)
+            {
+                throw new ArgumentNullException(nameof(textNumber));
+            }
+            else if (sourceBase <= 0 || sourceBase > maxBase)
+            {
+                string paramName = nameof(sourceBase);
+                throw new ArgumentOutOfRangeException(paramName, sourceBase, $"{paramName} needs be greater than zero");
+            }
+            else if (textNumber == String.Empty)
+            {
+                string paramName = nameof(textNumber);
+                throw new ArgumentException($"{paramName} must not be empty", paramName);
+            }
+            
+
+            foreach (char symbol in textNumber)
+            {
+                int currentNumericCharValue = -1;
+
+                if (Char.IsDigit(symbol))
+                {
+                    currentNumericCharValue = symbol - '0';
+
+                    if (currentNumericCharValue >= sourceBase)
+                    {
+                        return false;
+                    }
+                }
+                else if (Char.IsLetter(symbol))
+                {
+
+                    // Removes case sensitivity for letters.
+                    char upperLetter = Char.ToUpper(symbol);
+
+                    // For numbers of higher base than 10 only A-Z are accepted.
+                    if (upperLetter >= 'A' && upperLetter <= 'Z')
+                    {
+                        currentNumericCharValue = (int)(upperLetter - 'A') + 10;
+
+                        if (currentNumericCharValue >= sourceBase)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        string paramName = nameof(textNumber);
+                        throw new ArgumentException($"{paramName} has a not valid symbol", paramName);
+                    }
+                }
+                else
+                {
+                    string paramName = nameof(textNumber);
+                    throw new ArgumentException($"{paramName} has a not valid symbol", paramName);
+                }
+                
+            }
+
+            return true;
+        }
+
+        public static int NumberValueFromTextNumber(string textNumber, int sourceBase)
+        {
+
+            if (IsValidNumberWithBase(textNumber, sourceBase) == false)
+            {
+                return -1;
+            }
+            else if (sourceBase == 1)
+            {
+                return textNumber.Length - 1;
+            }
+
+            int numericValueOfTextNumber = 0;
+            double currentBaseMultiplier = 0.0;
+
+            for (var i = textNumber.Length - 1; i > -1; i--)
+            {
+                char symbol = textNumber[i];
+                int currrentSymbolPlaceValue;
+                if (Char.IsDigit(symbol))
+                {
+                    currrentSymbolPlaceValue = symbol - '0';
+                }
+                else
+                {
+                    currrentSymbolPlaceValue = (Char.ToUpper(symbol) - 'A') + 10;
+                }
+
+                numericValueOfTextNumber += currrentSymbolPlaceValue * (int)Math.Pow((double)sourceBase, currentBaseMultiplier);
+                currentBaseMultiplier++;
+            }
+            
+            return numericValueOfTextNumber;
+
+        }      
+        
+
+        public static string TextNumberFromNumberValue(int numericValue, int targetBase)
+        {
+            var textNumberReverted = new StringBuilder();
+            int currrentNumericValue = numericValue;
+
+            if (numericValue < 0)
+            {
+                string paramName = nameof(numericValue);
+                throw new ArgumentOutOfRangeException(paramName, numericValue, $"{paramName} must not be negative.");
+            }
+            else if (targetBase <= 0 || targetBase > MaxBase)
+            {
+                string paramName = nameof(targetBase);
+                throw new ArgumentOutOfRangeException(paramName, targetBase, $"{paramName} must be greater than zero.");
+            }
+
+            StringBuilder textNumber;
+
+            if (targetBase == 1)
+            {
+                textNumber  = new StringBuilder(numericValue + 1);
+                for (var i = 0; i < numericValue + 1; i++)
+                {
+                    textNumber.Append('0');
+                }
+
+                return textNumber.ToString();
+            }
+
+
+
+
+            do
+            {
+                int currentNumberValue = currrentNumericValue % targetBase;
+                char currentChar;
+
+                if (currentNumberValue >= 0 && currentNumberValue < 10)
+                {
+                    currentChar = (char)('0' + currentNumberValue);
+                }
+                else
+                {
+                    currentChar = (char)('A' + (currentNumberValue - 10));
+                }
+
+                textNumberReverted.Append(currentChar);
+                currrentNumericValue /= targetBase;
+
+            } while (currrentNumericValue > 0);
+
+            textNumber = new StringBuilder(textNumberReverted.Length);
+            
+            for (var i = textNumberReverted.Length - 1; i > -1;  i-- )
+            {
+                textNumber.Append(textNumberReverted[i]);    
+            }
+
+
+            return textNumber.ToString();
+        }
+
+    }
+}
---
+++ NumberBaseConvertionLibraryUnitTest/NumberBaseConvertingTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xunit;
+using NumberBaseConvertionLibrary;
+
+namespace NumberBaseConvertionLibraryUnitTest
+{
+
+    public class NumberBaseConvertingTest
+    {
+        [Theory]
+        [InlineData("A4", 16, true)]
+        [InlineData("A4", 9, false)]
+        [InlineData("A4", 10, false)]
+        [InlineData("E", 15, true)]
+        [InlineData("E", 12, false)]
+        [InlineData("5", 3, false)]
+        [InlineData("5", 6, true)]
+        public static void IsValidNumberWithBase_ShouldTellIfNumberIsValidWithGivenBase(string textNumber, int sourceBase, bool expected)
+            => Assert.Equal(expected, NumberBaseConverting.IsValidNumberWithBase(textNumber, sourceBase));
+
+        [Fact]
+        public static void IsValidNumberWithBase_ShouldThrowWhenTextNumberIsNull() => Assert.Throws<ArgumentNullException>("textNumber", () => NumberBaseConverting.IsValidNumberWithBase(null, 2));
+
+        [Theory]
+        [InlineData("/*", 1)]
+        [InlineData("2-5", 6)]
+        [InlineData("\n", 6)]
+        public static void IsValidNumberWithBase_ShouldThrowWhenTextNumberHasInvalidSymbol(string unvalidTextNumber, int validBase)
+            => Assert.Throws<ArgumentException>("textNumber", () => NumberBaseConverting.IsValidNumberWithBase(unvalidTextNumber, validBase));
+
+
+        [Theory]
+        [MemberData(nameof(TestData.TestNegativeOrZeroBases), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.TestTooBigPositiveValue), MemberType = typeof(TestData))]
+        public static void IsValidNumberWithBase_ShouldThrowWhenBaseIsOutOfValidRange(int invalidBase)
+            => Assert.Throws<ArgumentOutOfRangeException>("sourceBase", () => NumberBaseConverting.IsValidNumberWithBase("1", invalidBase));
+
+
+        [Fact]
+        public static void IsValidNumberWithBase_ShouldThrowTextNumberIsEmpty() => Assert.Throws<ArgumentException>("textNumber", () => NumberBaseConverting.IsValidNumberWithBase("", 2));
+
+        [Theory]
+        [InlineData("1", 2, 1)]
+        [InlineData("0", 2, 0)]
+        [InlineData("8", 10, 8)]
+        [InlineData("A", 16, 10)]
+        [InlineData("A4", 16, 164)]
+        [InlineData("a4b", 16, 2635)]
+        [InlineData("45", 8, 37)]
+        [InlineData("1000110011", 2, 563)]
+        [InlineData("8", 7, -1)]
+        [InlineData("DE4A", 16, 56906)]
+        [InlineData("31", 4, 13)]
+        [InlineData("0000", 1, 3)]
+        [InlineData("0", 1, 0)]
+        [InlineData("Z", 36, 35)]
+
+        public static void NumberValueFromTextNumber_ShouldConvertTextNumberToNumericValueRight(string textNumber, int sourceBase, int expected)
+            => Assert.Equal(expected, NumberBaseConverting.NumberValueFromTextNumber(textNumber, sourceBase));
+
+        [Theory]
+        [InlineData(20, 2, "10100")]
+        [InlineData(0, 10, "0")]
+        [InlineData(10, 10, "10")]
+        [InlineData(74, 8, "112")]
+        [InlineData(852, 16, "354")]
+        [InlineData(840734, 16, "CD41E")]
+        [InlineData(3, 1, "0000")]
+        [InlineData(0, 1, "0")]
+        [InlineData(35, 36, "Z")]
+        [InlineData(37, 36, "11")]
+        public static void TextNumberFromNumberValue_ShouldConvertNumericValueToTextNumber(int numericValue, int targetBase, string expected)
+            => Assert.Equal(expected, NumberBaseConverting.TextNumberFromNumberValue(numericValue, targetBase));
+
+
+        [Theory]
+        [MemberData(nameof(TestData.TestNegativeOrZeroBases), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.TestTooBigPositiveValue), MemberType = typeof(TestData))]
+        public static void TextNumberFromNumberValue_ShouldThrowWhenTargetBaseOutOfValidRange(int invalidBase) => Assert.Throws<ArgumentOutOfRangeException>(() => NumberBaseConverting.TextNumberFromNumberValue(1, invalidBase));
+
+
+        private class TestData
+        {
+            public static IEnumerable<object[]> TestNegativeOrZeroBases => 
+                new List<object[]>
+                {
+                    new object[] {0},
+                    new object[] {-2},
+                    new object[] {int.MinValue}
+                };
+
+
+            public static IEnumerable<object[]> TestTooBigPositiveValue =>
+                new List<object[]>
+                {
+                    new object[] {NumberBaseConverting.MaxBase + 1},
+                    new object[] {NumberBaseConverting.MaxBase + 10},
+                    new object[] {int.MaxValue}
+                };
+        }
+
+    }   
+}
